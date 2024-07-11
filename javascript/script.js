74069b49ffaaa22fb04147bea42af0022512eb4a
KartikPawar531 <169176366+KartikPawar531@users.noreply.github.com>
2024-07-11 12:36:45 +0530

    third date
    date:11-07-24

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+{/* <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script> */}
+   
+    var typed = new Typed('#element', {
+      strings: ['Create memories that last a lifetime.',],
+      typeSpeed: 50,
+    });
+  
+
+    // <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js">
+    
+    var typed = new Typed('#element1', {
+      strings: [ 'The Whole World Awaits'],
+      typeSpeed: 50,
+    });
